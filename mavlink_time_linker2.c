170180c825a634fc3016f8fa40b94ff2fe024194
GiacomoFavaron <jack.favaron@gmail.com>
2021-09-03 13:00:51 +0200

    time linkers

--- mavlink_time_linker2.c
+++ mavlink_time_linker2.c
@@ -1,20 +1,14 @@
 #include <errno.h>
 #include <fcntl.h> 
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
-=======
->>>>>>> 0fdd7253c8f461aae58d68be6f774e7da1e8d77d
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
 
 #include <common/mavlink.h>
 #include <common/mavlink_msg_system_time.h>
-<<<<<<< HEAD
 #include <common/mavlink_msg_timesync.h>
-=======
->>>>>>> 0fdd7253c8f461aae58d68be6f774e7da1e8d77d
 
 int
 set_interface_attribs (int fd, int speed, int parity)
@@ -22,11 +16,7 @@ set_interface_attribs (int fd, int speed, int parity)
         struct termios tty;
         if (tcgetattr (fd, &tty) != 0)
         {
-<<<<<<< HEAD
                 fprintf (stderr, "error %d from tcgetattr", errno);
-=======
-                error_message ("error %d from tcgetattr", errno);
->>>>>>> 0fdd7253c8f461aae58d68be6f774e7da1e8d77d
                 return -1;
         }
 
@@ -54,11 +44,7 @@ set_interface_attribs (int fd, int speed, int parity)
 
         if (tcsetattr (fd, TCSANOW, &tty) != 0)
         {
-<<<<<<< HEAD
                 fprintf (stderr, "error %d from tcsetattr", errno);
-=======
-                error_message ("error %d from tcsetattr", errno);
->>>>>>> 0fdd7253c8f461aae58d68be6f774e7da1e8d77d
                 return -1;
         }
         return 0;
@@ -71,11 +57,7 @@ set_blocking (int fd, int should_block)
         memset (&tty, 0, sizeof tty);
         if (tcgetattr (fd, &tty) != 0)
         {
-<<<<<<< HEAD
                 fprintf (stderr, "error %d from tggetattr", errno);
-=======
-                error_message ("error %d from tggetattr", errno);
->>>>>>> 0fdd7253c8f461aae58d68be6f774e7da1e8d77d
                 return;
         }
 
@@ -83,16 +65,11 @@ set_blocking (int fd, int should_block)
         tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
 
         if (tcsetattr (fd, TCSANOW, &tty) != 0)
-<<<<<<< HEAD
                 fprintf (stderr, "error %d setting term attributes", errno);
-=======
-                error_message ("error %d setting term attributes", errno);
->>>>>>> 0fdd7253c8f461aae58d68be6f774e7da1e8d77d
 }
 
 //--------------------
 
-<<<<<<< HEAD
 int main() {
 
         mavlink_system_t mavlink_system = {
@@ -149,47 +126,3 @@ int main() {
         }
 
 }
-=======
-mavlink_status_t status;
-mavlink_message_t msg;
-int chan = MAVLINK_COMM_0;
-mavlink_system_time_t time_message;
-
-char *portname = "/dev/ttyS2"   // telem2 port
-
-int fd = open (portname, O_RDWR | O_NOCTTY | O_SYNC);
-if (fd < 0)
-{
-        error_message ("error %d opening %s: %s", errno, portname, strerror (errno));
-        return;
-}
-
-set_interface_attribs (fd, B9600, 0);  // set speed to 9600 bps, 8n1 (no parity)
-set_blocking (fd, 0);                // set no blocking
-
-//write (fd, "hello!\n", 7);           // send 7 character greeting
-
-//usleep ((7 + 25) * 100);             // sleep enough to transmit the 7 plus
-                                     // receive 25:  approx 100 uS per char transmit
-uint8_t byte = 0;
-//int n = read (fd, buf, sizeof buf);  // read up to 100 characters if ready to read
-
-while(1) {
-    int n = read (fd, &byte, 1);
-    if(n == -1) {
-        error_message ("error %d on read: %s", errno, portname, strerror (errno));
-        return;
-    }
-	if (mavlink_parse_char(chan, byte, &msg, &status)) {
-			printf("Received message with ID %d, sequence: %d from component %d of system %d\n", msg.msgid, msg.seq, msg.compid, msg.sysid);
-			switch(msg.msgid) {
-				case MAVLINK_MSG_ID_SYSTEM_TIME: {
-					mavlink_msg_system_time_decode(&msg, &time_message);
-					char string[20];
-					itoa(time_message.time_unix_usec,string,10);
-					printf("Unix Pixhawk time = %s\n", string);
-				}
-			}
-		}
-	}
->>>>>>> 0fdd7253c8f461aae58d68be6f774e7da1e8d77d
